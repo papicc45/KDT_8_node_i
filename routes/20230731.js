15c9bafcc832a26ce1c565d3a70da22f3117300d
papicc45 <papicc45@naver.com>
2023-07-31 13:55:01 +0900

    20230731

---
+++ routes/20230731.js
@@ -0,0 +1,89 @@
+// 구조분해
+// const lists = ['apple', 'grape'];
+//
+// [item1, item2] = lists;
+// console.log(item1, item2);
+// 인텔리제이에서 위에것만 안됨..
+
+//교환
+// let x = 1, y = 3;
+// [x, y] = [y, x];
+// console.log(x, y);
+
+//객체
+const Person = {
+    name: 'DongJun',
+    age: 27,
+    gender: 'M',
+    friends: ['Alex', 'Kevin'],
+    hello: () => {
+        console.log('hello');
+    },
+    "j-1": "America",
+}
+
+console.log(Person.name);
+console.log(Person['name']); //위와 같음
+Person.age = 29;
+console.log(Person.age);
+Person.hello();
+console.log(Person['j-1']) //키 값이 문자열일때 보통 사용(특수문자)
+Person.city = 'Seoul';  //객체 필드와 값 추가
+console.log(Person);
+
+//객체 구조분해
+const {name, city, gender, key1 = 'Hi'} = Person;  //동일한 키값을 가져옴
+console.log(name, city, gender, key1);
+
+
+//spread 연산자
+const a = [1, 2, 3];
+const b = [4, 5];
+const spread = [...a, ...b];
+console.log(spread);
+
+const c = [...'hello'];  //문자열은 인덱스단위 쪼개서 배열형식
+console.log(c);
+
+const person2 = {
+    name: 'DongJun',
+    age: 27,
+};
+
+console.log({...person2});
+
+console.log('--------------------------------');
+
+//rest
+
+function get(a, ...rest) {
+    console.log(a);
+    console.log(rest);
+}
+
+get(10, 20, 30, 40, 50);
+
+//클래스
+
+console.log('--------------------------------');
+
+class Cat {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    mew() {
+        console.log('야옹');
+    }
+
+    eat() {
+        console.log('먹이를 먹습니다.');
+    }
+}
+
+const cat1 = new Cat('나비', 1);
+const cat2 = new Cat('길냥', 3);
+console.log(cat1);
+cat1.mew();
+
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ import path from 'path'
 const app = express();
 const PORT = 8000;
 const __dirname = path.resolve('../');
-//
-// //뷰 엔진
+
+//뷰 엔진
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
@@ -13,16 +13,38 @@ app.set('views', __dirname + '/views');
 // app.use('public', express.static(__dirname + '/public/stylesheets/style.css'));
 // app.use(express.static('public'));
 app.use('/static', express.static(__dirname + '/public'));
+
+//body_parser
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 app.get('/', (req, res) => {
     // res.send({result : true, code : 1000, message : '회원가입에 성공하였습니다.'});
     // res.render('index', {data : [2, 3, 4, 5, 6, 7, 8, 9]});
-    res.render('index');
+    res.render('index', {title: '폼 실습'});
 })
+app.post('/', (req, res) => {
+    console.log(req.body);
+});
 
+app.get('/getForm', (req, res) => {
+    console.log(req.query);
+    res.render('result', {
+        title: 'GET 요청 폼 결과 확인하기',
+        userInfo: req.query
+    });
+});
 
+app.post('/postForm', (req, res) => {
+    console.log(req.body);
+    res.render('result', {
+        title: 'POST 요청 폼 결과 확인하기',
+        userInfo: req.body
+    });
+});
 
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
 
-console.log(`http://localhost:${PORT}`);
-console.log(__dirname);
+    console.log(`http://localhost:${PORT}`);
+    console.log(__dirname);
 })
